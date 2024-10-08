cabe6126f57e6db01ce4626ae9de179471bd7ad9
Hamuko11 <coldrain111@gmail.com>
2024-10-08 16:22:53 +0900

    4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,37 +11,52 @@ use App\Http\Requests\ProductStoreRequest;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         //productsテーブルからすべてを取得し変数に代入
         $products = DB::table('products')->get();
         //変数をindex.blade.phpに渡す
         //compactは変数名とその値から配列を作成する
-        return view('products.index',compact('products'));
+        return view('products.index', compact('products'));
     }
     //$idのページを見る
-    public function show($id){
+    public function show($id)
+    {
         //URL'/products/{id}'の{id}部分とidカラムの値が一致
         //するデータをproductsテーブルから取得し変数に代入する
         $product = Product::find($id);
         //変数をproducts/show.blade.phpに渡す
-        return view('products.show',compact('product'));
+        return view('products.show', compact('product'));
     }
 
-    public function create() {
-        $vendor_codes=Vendor::pluck('vendor_code');
+
+
+    public function create()
+    {
+        $vendor_codes = Vendor::pluck('vendor_code');
 
         return view('products.create', compact('vendor_codes'));
     }
 
-    public function store(ProductStoreRequest $request){
-        //フォームの入力をもとにテーブルにデータを追加する
+    public function store(ProductStoreRequest $request)
+    {
+        // フォームの入力内容をもとに、テーブルにデータを追加する
         $product = new Product();
-        $product->product_name=$request->input('product_name');
-        $product->price=$request->input('price');
-        $product->vendor_code=$request->input('vendor_code');
+        $product->product_name = $request->input('product_name');
+        $product->price = $request->input('price');
+        $product->vendor_code = $request->input('vendor_code');
+
+        // アップロードされたファイル（name="image"）が存在すれば処理を実行する
+        if ($request->hasFile('image')) {
+            // アップロードされたファイル（name="image"）をstorage/app/public/productsフォルダに保存し、戻り値（ファイルパス）を変数$image_pathに代入する
+            $image_path = $request->file('image')->store('public/products');
+            // ファイルパスからファイル名のみを取得し、Productインスタンスのimage_nameプロパティに代入する
+            $product->image_name = basename($image_path);
+        }
+
         $product->save();
 
-        //リダイレクトする
+        // リダイレクトさせる
         return redirect("/products/{$product->id}");
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
- <html lang="ja">
- 
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Laravel基礎</title>
- </head>
- 
- <body>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel基礎</title>
+</head>
+
+<body>
   <table>
     <tr>
       <th>ID</th>
@@ -29,7 +29,13 @@
       <th>更新日時</th>
       <td>{{$product->updated_at}}</td>
     </tr>
-    </table>
- </body>
- 
- </html>
+    <tr>
+      <th>画像</th>
+      <td>
+        <img src="{{asset('/storage/products/'.$product->image_name)}}" alt="アップロードした画像ファイル">
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
